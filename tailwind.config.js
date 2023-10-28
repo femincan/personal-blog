aee1a0b9edb573db887637fee04df86f91f57395
Furkan Emin Can <femincanx@gmail.com>
2023-10-28 11:56:05 +0300

    feat: add tailwind and solid-js integration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  /** @type {import('daisyui').Config} */
+  daisyui: {
+    themes: ['forest'],
+  },
+};
