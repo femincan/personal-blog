b45022d2a5556358ab8be81cec2fe8cab7f95c6d
Furkan Emin Can <femincanx@gmail.com>
2023-10-28 11:56:19 +0300

    feat: add Post type

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Post = {
+  slug: string;
+  title: string;
+  author: {
+    name: string;
+  };
+  publishedAt: string;
+  url: string;
+  coverImage: {
+    url: string;
+  };
+  content: {
+    markdown: string;
+  };
+};
