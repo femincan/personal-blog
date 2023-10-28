8edafe11dd930a114596c0453af3ac1b0be0f9fe
Furkan Emin Can <femincanx@gmail.com>
2023-10-28 11:58:23 +0300

    feat: convertMarkdown utility function

---
+++ src/utils/convertMarkdown.ts
@@ -0,0 +1,22 @@
+import rehypeHighlight from 'rehype-highlight';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
+
+const convertMarkdown = async (content: string) => {
+  const cleanedContent = content.replaceAll(' align="center"', '');
+
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeHighlight)
+    .use(rehypeStringify)
+    .process(cleanedContent);
+
+  return String(file);
+};
+
+export default convertMarkdown;
