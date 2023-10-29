c40c3abedcad2a4ff1f5e89f20452f4dab83b1b1
Furkan Emin Can <femincanx@gmail.com>
2023-10-29 08:07:37 +0300

    chore: add hashnode cdn to image domains config property

--- astro.config.js
+++ astro.config.js
@@ -5,6 +5,7 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+  image: { domains: ['cdn.hashnode.com'] },
   integrations: [
     tailwind({
       applyBaseStyles: false,
