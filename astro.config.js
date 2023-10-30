8fd4206acbb5ce7f119fe2eb73bfa11007e86647
Furkan Emin Can <femincanx@gmail.com>
2023-10-30 17:58:29 +0300

    feat: add sitemap

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://headless-personal-blog.netlify.app',
-  image: { domains: ['cdn.hashnode.com'] },
+  image: {
+    domains: ['cdn.hashnode.com'],
+  },
   integrations: [
+    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
