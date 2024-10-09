1a5c431b218d4800096ca98d731a07371b0b4014
Furkan Emin Can <femincanx@gmail.com>
2024-10-09 10:14:38 +0300

    style: use new flat config for eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 'off',
+    },
+  },
+  { ignores: ['dist', 'tailwind.config.cjs'] },
+);
